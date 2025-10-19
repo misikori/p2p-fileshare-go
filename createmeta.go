2cc6af7e2fa154230d16df2d3657206fbbe1e74c
Milos Obrenovic <obrenovicmilos01@gmail.com>
2025-10-19 02:56:18 +0200

    feat: metadata file creation

---
+++ createmeta.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"p2p-fileshare/metafile"
+)
+
+func main() {
+	meta, err := metafile.Create("http://tracker:8080", "./shared/dummy_file.txt")
+	if err != nil {
+		log.Fatalf("Error creating meta file: %v", err)
+	}
+
+	f, _ := os.Create("./shared/dummy_file.txt.meta")
+	defer f.Close()
+	json.NewEncoder(f).Encode(meta)
+	log.Println("Created ./shared/dummy_file.txt.meta")
+}
